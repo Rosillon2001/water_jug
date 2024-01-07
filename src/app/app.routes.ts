f96ded442cf9f8e7dcad850597bf1c4ec451fba7
Daniel Rosillon <rosillon2001@gmail.com>
2024-01-07 10:06:40 -0400

    Updated the app routing
    Updated app routing using router outlet

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+// Water Jugs Component
+import { WaterJugsComponent } from './components/water-jugs/water-jugs.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: WaterJugsComponent },
+  { path: '**', redirectTo: '/'}
+];
